400d18ca024f6dd411e392c53bd2f6b0311d05bf
Siam Ahmed <69627001+siamsplash5@users.noreply.github.com>
2023-03-02 08:56:57 +0600

    Add files via upload

--- srcipt.js
+++ srcipt.js
@@ -4,10 +4,20 @@ const Increment = () => {
     const [counter, setCounter] = React.useState(0);
     return (
         <div class="container mt-5 pt-5">
-            <h1 id="display">{counter}</h1>
-            <button class="btn btn-primary" onClick={() => setCounter(counter + 1)} >Increment + </button>
+            <div id="circle">
+                <div id="display">
+                    <h1>{counter}</h1>               
+                    <p>Steps</p>
+                </div>
+            </div>
+            <button class="btn btn-success ml-2"
+                onClick={() => setCounter(counter + 1)} > + </button>
+            <button class="btn btn-danger m-2"
+                onClick={() => { if (confirm('Do you want to reset?')) setCounter(0); }}>Reset</button>
+            <button class="btn btn-warning"
+                onClick={() => { (counter > 1) ? setCounter(counter - 1) : setCounter(0) }}>-</button>
         </div>
     );
 }
 
-ReactDOM.render(<Increment/>,domContainer);
+ReactDOM.render(<Increment />, domContainer);
